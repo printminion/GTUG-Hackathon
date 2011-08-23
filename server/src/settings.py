275a88be64070f25b4f653e708f235f18dde5f42
Johannes Seitz <JohannesSeitz@freenet.de>
2011-08-23 20:52:46 +0200

    Settings local added

--- server/src/settings.py
+++ server/src/settings.py
@@ -3,3 +3,8 @@
 CLIENT_ID='982267452290-ag21de8eeim6rsbh47up3sopl6q3d526.apps.googleusercontent.com'
 CLIENT_SECRET='rdDNNMTUswKpnBEHzo2OvmOx'
 SCOPE='https://www.googleapis.com/auth/tasks'
+
+try:
+    from settings_local import *
+except:
+    pass
