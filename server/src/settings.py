39b265582485dc22d9cac7cdff1042b3b837f744
Johannes Seitz <JohannesSeitz@freenet.de>
2011-08-23 20:41:19 +0200

    Fixed Client ID and Client Secet

--- server/src/settings.py
+++ server/src/settings.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-CLIENT_ID='982267452290.apps.googleusercontent.com'
-CLIENT_SECRET='uKKpyPzTL-C6Nz-L9_cxIVe2'
+CLIENT_ID='982267452290-ag21de8eeim6rsbh47up3sopl6q3d526.apps.googleusercontent.com'
+CLIENT_SECRET='rdDNNMTUswKpnBEHzo2OvmOx'
 SCOPE='https://www.googleapis.com/auth/tasks'
